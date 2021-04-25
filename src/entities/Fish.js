37e89066ff239021f08ca1769e5180072ae9c2bd
Cary Stanley <cary@yahoo-inc.com>
2021-04-24 19:56:36 -0700

    Add Fish Settings

--- src/entities/Fish.js
+++ src/entities/Fish.js
@@ -1,6 +1,13 @@
-const XVELOCITY = 16;
 const RIGHT = 1;
 const LEFT = -1;
+
+const fishSettings = {
+    default: { w: 16, h: 16, countMin: 2, countRange: 5, vel: 16 },
+    'piranha-fish': { w: 16, h: 16, countMin: 2, countRange: 5, vel: 65 },
+    'angler-fish': { w: 16, h: 16, countMin: 2, countRange: 5, vel: 65 },
+    'clown-fish': { w: 16, h: 16, countMin: 2, countRange: 5, vel: 10 },
+    'crab': { w: 16, h: 16, countMin: 2, countRange: 5, vel: 10, gravity: 20 }
+}
 
 export default class Fish extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, type) {
@@ -8,10 +15,16 @@ export default class Fish extends Phaser.Physics.Arcade.Sprite {
         scene.physics.add.existing(this);
         scene.add.existing(this);
 
+        this.settings = fishSettings[type] || fishSettings.default;
+
         this.setMaxVelocity(500, 500)
-            .setSize(16, 16)
+            .setSize(this.settings.w, this.settings.h)
             .setOffset(0, 0)
             .setCollideWorldBounds(true);
+
+        if (this.settings.gravity) {
+            this.body.gravity.y = this.settings.gravity;
+        }
 
         this.setMovement();
 
@@ -22,8 +35,8 @@ export default class Fish extends Phaser.Physics.Arcade.Sprite {
     setMovement() {
         this.direction = (Math.random() > 0.5) ? LEFT : RIGHT;
         this.setFlipX((this.direction === RIGHT));
-        this.setVelocity(XVELOCITY * this.direction, 0)
-        this.swimCounter = Math.random() * 5 + 2 * 1000;
+        this.setVelocity(this.settings.vel * this.direction, 0)
+        this.swimCounter = Math.random() * this.settings.countRange + this.settings.countMin * 1000;
     }
 
     update(time, delta) {
