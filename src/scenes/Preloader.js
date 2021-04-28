30770b0c4c5d3d5e4c41542aa6ebb380ebef94bb
Cary Stanley <cary@yahoo-inc.com>
2021-04-28 13:32:20 -0700

    Add found tiles

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -24,6 +24,7 @@ class Preloader extends Phaser.Scene {
         this.load.spritesheet('fish16', 'fish16.png', { frameWidth: 16, frameHeight: 16 });
         this.load.spritesheet('shark', 'shark.png', { frameWidth: 32, frameHeight: 32 });
         this.load.spritesheet('o2tank', 'o2tank.png', { frameWidth: 16, frameHeight: 32 });
+        this.load.spritesheet('collection', 'collection.png', { frameWidth: 12, frameHeight: 12 });
         this.load.tilemapTiledJSON('world', 'world.json');
 
         this.load.audio('hit', ['sfx/hit.mp3', 'sfx/hit.ogg']);
@@ -55,7 +56,19 @@ class Preloader extends Phaser.Scene {
             { key: 'minnow-fish', image: 'fish16', start: 12, end: 13, frameRate: 3, repeat: -1 },
             { key: 'crab', image: 'fish16', start: 14, end: 15, frameRate: 3, repeat: -1 },
             { key: 'rainbow-fish', image: 'fish16', start: 16, end: 17, frameRate: 3, repeat: -1 },
-            { key: 'shark', image: 'shark', start: 0, end: 0, frameRate: 3, repeat: -1 }
+            { key: 'shark', image: 'shark', start: 0, end: 0, frameRate: 3, repeat: -1 },
+
+            { key: 'found-red-fish', image: 'collection', start: 0, end: 0, frameRate: 3, repeat: -1 },
+            { key: 'found-blue-fish', image: 'collection', start: 1, end: 1, frameRate: 3, repeat: -1 },
+            { key: 'found-piranha-fish', image: 'collection', start: 2, end: 2, frameRate: 3, repeat: -1 },
+            { key: 'found-angler-fish', image: 'collection', start: 3, end: 3, frameRate: 3, repeat: -1 },
+            { key: 'found-duck-fish', image: 'collection', start: 4, end: 4, frameRate: 3, repeat: -1 },
+            { key: 'found-clown-fish', image: 'collection', start: 5, end: 5, frameRate: 3, repeat: -1 },
+            { key: 'found-minnow-fish', image: 'collection', start: 6, end: 6, frameRate: 3, repeat: -1 },
+            { key: 'found-crab', image: 'collection', start: 7, end: 7, frameRate: 3, repeat: -1 },
+            { key: 'found-rainbow-fish', image: 'collection', start: 8, end: 8, frameRate: 3, repeat: -1 },
+            { key: 'found-shark', image: 'collection', start: 9, end: 9, frameRate: 3, repeat: -1 },
+            { key: 'found-empty', image: 'collection', start: 10, end: 10, frameRate: 3, repeat: -1 }
         ]);
 
         this.scene.start('play');
